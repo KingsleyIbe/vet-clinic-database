8f189f2b089861aa32db0f16a9dd6ec2c660e729
kingsleyibe <ibekingsley66@gmail.com>
2022-02-15 05:33:37 +0100

    Initial commit on branch

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE animals( 
+id INT PRIMARY KEY NOT NULL, 
+name            TEXT       NOT NULL, 
+date_of_birth   DATE       NOT NULL, 
+escape_attempts INT        NOT NULL, 
+neutered        BOOL       NOT NULL, 
+weight_kg       REAL       NOT NULL 
+);
