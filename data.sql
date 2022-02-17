e399908fe13121a639a02318f41e0ca9d5674474
kingsleyibe <ibekingsley66@gmail.com>
2022-02-17 09:33:39 +0100

    Update data.sql and schema.sql files

--- data.sql
+++ data.sql
@@ -54,10 +54,14 @@ UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
 
 
-/* INSERT DATA INTO THE OWNERS TABLE*/
+/* INSERT DATA INTO THE 'owners' TABLE */
 INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
 INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
 INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
 INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
 INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
 INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+
+/* INSERT DATA INTO THE 'species' TABLE */
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
