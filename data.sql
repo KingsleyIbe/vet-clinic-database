5cd27ac2ab7f1e2010175cbdb8236fc7d35147f5
kingsleyibe <ibekingsley66@gmail.com>
2022-02-17 10:23:13 +0100

    Refactor code

--- data.sql
+++ data.sql
@@ -1,16 +1,23 @@
 /* ADDED TABLE DATA */
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (1, 'Agumon',  '02-03-2020', 0, true, 10.23 );
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (2, 'Gabumon',  '15-11-2018', 2, true, 8.00 );
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (3, 'Pikachu',  '01-07-2021', 2, false, 15.04 );
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (4, 'Devimon',  '05-12-2017', 5, true, 11.00 );
+INSERT INTO 
+  animals
+    (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+  VALUES (1, 'Agumon',  '02-03-2020', 0, true, 10.23 ),
+    (2, 'Gabumon',  '15-11-2018', 2, true, 8.00 ),
+    (3, 'Pikachu',  '01-07-2021', 2, false, 15.04 ),
+    (4, 'Devimon',  '05-12-2017', 5, true, 11.00 );
 
 /* ADDED MORE TABLE DATA */
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (5, 'Charmander',  '08-02-2020', 0, false, 11.00 );
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (6, 'Plantmon',  '15-11-2022', 2, true, -5.70 );
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (7, 'Squirtle',  '02-04-1993', 3, false, -12.13 );
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (8, 'Angemon',  '12-06-2005', 1, true, -45.00 );
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (9, 'Boarmon',  '07-06-2005', 7, true, 20.40 );
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (10, 'Blossom',  '13-10-1998', 3, true, 17.00 );
+INSERT INTO 
+  animals 
+    (id, name, date_of_birth, escape_attempts, neutered, weight_kg) 
+  VALUES 
+    (5, 'Charmander',  '08-02-2020', 0, false, 11.00 ),
+    (6, 'Plantmon',  '15-11-2022', 2, true, -5.70 ),
+    (7, 'Squirtle',  '02-04-1993', 3, false, -12.13 ),
+    (8, 'Angemon',  '12-06-2005', 1, true, -45.00 ),
+    (9, 'Boarmon',  '07-06-2005', 7, true, 20.40 ),
+    (10, 'Blossom',  '13-10-1998', 3, true, 17.00 );
 
 /* WORKING WITH TRANSACTION */
 /* UPDATING SPECIES COLUMN WITH 'unspecified' WITH A ROLLBACK TO REVERT PREVIOUS STATE*/
@@ -55,16 +62,22 @@ COMMIT;
 
 
 /* INSERT DATA INTO THE 'owners' TABLE */
-INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
-INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
-INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
-INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
-INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
-INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+INSERT INTO 
+  owners 
+    (full_name, age)
+  VALUES ('Sam Smith', 34),
+   ('Jennifer Orwell', 19),
+   ('Bob', 45),
+   ('Melody Pond', 77),
+   ('Dean Winchester', 14),
+   ('Jodie Whittaker', 38);
 
 /* INSERT DATA INTO THE 'species' TABLE */
-INSERT INTO species (name) VALUES ('Pokemon');
-INSERT INTO species (name) VALUES ('Digimon');
+INSERT INTO 
+  species 
+    (name) 
+  VALUES ('Pokemon'),
+    ('Digimon');
 
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 
--- queries.sql
+++ queries.sql
@@ -1,34 +1,77 @@
 /* Query Data to obtain specific information */
-SELECT date_of_birth FROM animals WHERE name LIKE '%mon';
-SELECT name FROM animals WHERE date_of_birth BETWEEN '01-01-2016' AND '31-12-2019';
-SELECT name FROM animals WHERE neutered IS true AND escape_attempts < 3;
-SELECT name FROM animals WHERE name IN ('Agumon', 'Pikachu');
-SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.50;
-SELECT name FROM animals WHERE neutered IS true;
-SELECT name FROM animals WHERE name != ('Gabumon');
-SELECT name FROM animals WHERE (weight_kg >= 10.40 AND weight_kg <= 17.30);
+SELECT date_of_birth 
+  FROM animals 
+  WHERE name LIKE '%mon';
+
+SELECT name 
+  FROM animals 
+  WHERE date_of_birth BETWEEN '01-01-2016' AND '31-12-2019';
+
+SELECT name 
+  FROM animals 
+  WHERE neutered IS true AND escape_attempts < 3;
+
+SELECT name 
+  FROM animals 
+  WHERE name IN ('Agumon', 'Pikachu');
+
+SELECT name, escape_attempts 
+  FROM animals 
+  WHERE weight_kg > 10.50;
+
+SELECT name 
+  FROM animals 
+  WHERE neutered IS true;
+
+SELECT name 
+  FROM animals 
+  WHERE name != ('Gabumon');
+
+SELECT name 
+  FROM animals 
+  WHERE (weight_kg >= 10.40 AND weight_kg <= 17.30);
 
 /* Write queries to answer the following questions */
 /* How many animals are there? */
-SELECT COUNT(name) FROM animals;
+SELECT COUNT(name) 
+  FROM animals;
 
 /* How many animals have never tried to escape? */
-SELECT COUNT(name) FROM animals WHERE escape_attempts = 0;
+SELECT COUNT(name) 
+  FROM animals 
+  WHERE escape_attempts = 0;
 
 /* What is the average weight of animals? */
-SELECT AVG(weight_kg) FROM animals;
+SELECT AVG(weight_kg) 
+  FROM animals;
 
 /* Who escapes the most, neutered or not neutered animals? */
-SELECT MAX(escape_attempts) FROM animals;
-SELECT COUNT(name) FROM animals WHERE neutered = '0';
-SELECT COUNT(name) FROM animals WHERE neutered = '1';
+SELECT MAX(escape_attempts) 
+  FROM animals;
+
+SELECT COUNT(name) 
+  FROM animals 
+  WHERE neutered = '0';
+
+SELECT COUNT(name) 
+  FROM animals 
+  WHERE neutered = '1';
 
 /* What is the minimum and maximum weight of each type of animal? */
-SELECT MIN(weight_kg) FROM animals GROUP BY species; 
-SELECT MAX(weight_kg) FROM animals GROUP BY species; 
+SELECT MIN(weight_kg) 
+  FROM animals 
+  GROUP BY species;
+
+SELECT MAX(weight_kg) 
+  FROM animals 
+  GROUP BY species; 
 
 /* What is the average number of escape attempts per animal type of those born between 1990 and 2000? */
-SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '01-01-1990' AND '31-12-2000'  GROUP BY species;
+SELECT AVG(escape_attempts) 
+  FROM animals 
+  WHERE date_of_birth 
+  BETWEEN '01-01-1990' AND '31-12-2000'  
+  GROUP BY species;
 
 /* WRITE QUERIES USING JOIN TO ANSWER THE FOLLOWING QUESTIONS: */
 
